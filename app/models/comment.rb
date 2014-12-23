dfa7bfd08f485a5951c0d8313bd997a94d76e6be
Alex Izotov <martinandrudy@gmail.com>
2014-12-23 19:45:08 +0300

    converted tabs of 4 spaces to 2 spaces
    Signed-off-by: Alex Izotov <martinandrudy@gmail.com>

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,21 +1,21 @@
 class Comment < ActiveRecord::Base
-    after_create :email_article_author
-    after_create :send_comment_email
+  after_create :email_article_author
+  after_create :send_comment_email
 
-    belongs_to :article
+  belongs_to :article
 
-    validates_presence_of :name, :email, :body
-    validate :article_should_be_published
+  validates_presence_of :name, :email, :body
+  validate :article_should_be_published
 
-    def article_should_be_published
-        errors.add(:article_id, "is not published yet") if article && !article.published?
-    end
+  def article_should_be_published
+    errors.add(:article_id, "is not published yet") if article && !article.published?
+  end
 
-    def email_article_author
-        puts "We will notify #{article.user.email} in Chapter 9"
-    end
+  def email_article_author
+    puts "We will notify #{article.user.email} in Chapter 9"
+  end
 
-    def send_comment_email
-        Notifier.comment_added(self).deliver
-    end
+  def send_comment_email
+    Notifier.comment_added(self).deliver
+  end
 end
