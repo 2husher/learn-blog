e1e2ab5b0b13f8789624693236d7c92f228673e9
Alex Izotov <martinandrudy@gmail.com>
2014-12-23 19:59:57 +0300

    updated scopes notation in Article and Category
    Signed-off-by: Alex Izotov <martinandrudy@gmail.com>

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,10 +5,10 @@ class Article < ActiveRecord::Base
 
   validates_presence_of :title, :body
 
-  scope :published,   lambda { where("articles.published_at IS NOT NULL") }
-  scope :draft,       lambda { where("articles.published_at IS NULL") }
-  scope :recent,      lambda { published.where("articles.published_at > ?", 1.week.ago.to_date) }
-  scope :where_title, lambda { |term| where("articles.title LIKE ?", "%#{term}%") }
+  scope :published,   -> { where("published_at IS NOT NULL") }
+  scope :draft,       -> { where("published_at IS NULL") }
+  scope :recent,      -> { published.where("published_at > ?", 1.week.ago.to_date) }
+  scope :where_title, -> (term) { where("title LIKE ?", "%#{term}%") }
 
   def long_title
     "#{title} - #{published_at}"
